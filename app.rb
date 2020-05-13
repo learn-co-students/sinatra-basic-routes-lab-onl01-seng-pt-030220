945cbce6ccb147b5171bb5fe2bfb3b2018ca5ab1
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-13 04:01:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+      "My name is Ahmed Afandy"
+  end
+  get  '/hometown'  do
+    "My hometown is earth"
+  end
+  get  '/favorite-song'  do
+   "My favorite song is Aylin"
+  end
 end
