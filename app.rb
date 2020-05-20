4b06adf0f221fdee3c4ae22f7bfd7847f6534958
Austin Redmond <aredmond85@gmail.com>
2020-05-20 17:21:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Austin."
+  end
+
+  get '/hometown' do
+    "My hometown is San Antonio, TX."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Sound of Silence, by Disturbed."
+  end
 end
