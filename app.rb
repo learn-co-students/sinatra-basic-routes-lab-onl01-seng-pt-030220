9024cfac04a294e6be34c2e44e3d4bdfcbf4b0e0
Sara Garcia <sarisgar28@gmail.com>
2020-06-03 14:06:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        " My name is Sara"
+    end 
+
+    get '/hometown' do 
+        "My hometown is Cali, Colombia"
+    end 
+
+    get '/favorite-song' do 
+       "My favorite song is Snow, RHCP "
+    end 
 end
